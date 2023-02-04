f5a7a039d12fc238ed6e16762ff36c8bbbe2396d
Matthew Grillo <mwgrillo@vt.edu>
2023-02-04 18:03:04 -0500

    stylized forms

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
+proposals = []
 
 @app.route('/', methods=['GET'])
 def index():
@@ -12,6 +14,16 @@ def volunteer():
 @app.route('/propose', methods=['GET'])
 def propose():
     return render_template('propose.html')
+
+@app.route('/add', methods=['GET','POST'])
+def post_add():
+    first_name = request.form['first_name']
+    print("Received %s"%first_name)
+    return redirect('/success')
+
+@app.route('/success', methods=['GET'])
+def success():
+    return render_template('index.html', success=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
